0b0e8d7a40b759a296aab030e5b5b4d3890e71a0
Josue Lacerda <josuelacerda8@gmail.com>
2023-05-04 21:01:37 -0300

    Rails watch list

---
+++ app/models/bookmark.rb
@@ -0,0 +1,10 @@
+class Bookmark < ApplicationRecord
+    validates :comment, presence: true, length: { minimum: 6 }
+    validates :movie, presence: true
+    validates :list, presence: true
+    validates :movie_id, uniqueness: { scope: :list_id }
+  
+    belongs_to :movie
+    belongs_to :list
+end
+  
